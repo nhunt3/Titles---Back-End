584eeac6d751f2f367789cad59f0ad42c1f0b6a0
Nick Hunt <nhunt11@gmail.com>
2019-08-08 18:29:51 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient
-const bodyParser= require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
-let db;
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser= require('body-parser');
+var cors = require('cors');
 
-MongoClient.connect('mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge', (err, client) => {
+let db;
+const connectionString = 'mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge';
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+MongoClient.connect(connectionString, { useNewUrlParser: true }, (err, client) => {
     if (err) console.log(err);
     db = client.db('dev-challenge');
-    app.listen(3001, function() {
+    app.listen(3001, () => {
         console.log('listening on 3001');
     });
 })
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.get('/allTitles', (req, res, next) => {
+app.get('/allTitles', (req, res) => {
     db.collection('Titles').find(
-        {"TitleName": new RegExp(req.query.searchText)},
+        {"TitleName": new RegExp(req.query.searchText, 'i')},
         { projection: { _id: 0, TitleName: 1, TitleId: 1 } } )
         .toArray(function(err, results) {
             res.send(results); 
     });
 });
 
-app.get('/oneTitle', (req, res, next) => {
+app.get('/oneTitle', (req, res) => {
     db.collection('Titles').findOne({"TitleId": parseInt(req.query.id)}, function(err, results) {
        res.send(results);
     });
